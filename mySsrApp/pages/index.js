1f56c83841fcc70dfcc675e69a309f6b5f853e61
Arjit <arjit.sharma@gmail.com>
2021-02-09 19:23:36 +0000

    added an api call in getInitialProps

--- mySsrApp/pages/index.js
+++ mySsrApp/pages/index.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import DigitalClock from '../src/DigitalClock';
+import axios from 'axios';
 
 const Index = function (props) {
   const [currentTime, setCurrentTimeWin] = useState(
     props.time // new Date().toLocaleString()
   );
+
+  const [speakersData, setSpeakersData] = useState({
+    hasErrored: props.hasErrored,
+    message: props.message,
+    speakersData: props.speakersData,
+  });
 
   const tick = () => {
     setCurrentTimeWin(() => {
@@ -17,24 +24,54 @@ const Index = function (props) {
   }, []);
 
   return (
-    <h1>
-      <DigitalClock time={currentTime} />
-    </h1>
+    <>
+      <h1>
+        <DigitalClock time={currentTime} />
+      </h1>
+      <ul>
+        {speakersData.speakersData.map((speaker) => {
+          return (
+            <li key={speaker.id}>
+              {speaker.firstName} hrllo {speaker.lastName}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
 Index.getInitialProps = function () {
+  let speakers = axios
+    .get('http://localhost:4000/speakers')
+    .then((response) => {
+      return {
+        hasErrored: 'false',
+        speakersData: response.data,
+        time: new Date().toLocaleString(),
+      };
+    })
+    .catch(() => {
+      return {
+        hasErrored: 'false',
+        speakersData: error.message,
+        time: new Date().toLocaleString(),
+      };
+    });
+
+  return speakers;
+
   // return {
   //   time: new Date().toLocaleString(),
   // };
 
-  return new Promise((resolve, reject) => {
-    setInterval(() => {
-      resolve({
-        time: new Date().toLocaleString(),
-      });
-    }, 3000);
-  });
+  // return new Promise((resolve, reject) => {
+  //   setInterval(() => {
+  //     resolve({
+  //       time: new Date().toLocaleString(),
+  //     });
+  //   }, 3000);
+  // });
 };
 
 export default Index;
