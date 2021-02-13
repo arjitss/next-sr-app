a19649d4d4b5c06b0220f64109263f6430cce19a
Arjit <arjit.sharma@gmail.com>
2021-02-13 20:57:06 +0000

    Add production configuration

--- mySsrApp/server.js
+++ mySsrApp/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const next = require('next');
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV != 'production';
+console.log(dev);
+console.log(process.env.NODE_ENV);
 const app = next({ dev });
 
 const handle = app.getRequestHandler();
